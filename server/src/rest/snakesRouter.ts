6d3fe60108f9d86624ae4bc12056fefdd3e62404
Daniel Steuernol <dlsteuer@gmail.com>
2018-03-02 18:13:03 -0800

    add in bounty snakes

--- server/src/rest/snakesRouter.ts
+++ server/src/rest/snakesRouter.ts
@@ -18,5 +18,85 @@ router.get('/', ensureAuthenticated, async (req: express.Request, res: express.R
         url: "https://dsnek.herokuapp.com"
     })
 
+    snakes.push({
+        name: "Accio Bounty Snake",
+        url: "http://52.40.184.186:9001"
+    })
+
+    snakes.push({
+        name: "AppColony Bounty Snake",
+        url: "http://appcolony-battlesnake.herokuapp.com/"
+    })
+
+    snakes.push({
+        name: "Redbrick Bounty Snake",
+        url: "http://bountysnake2018.rdbrck.com/"
+    })
+
+    snakes.push({
+        name: "Checkfront Bounty Snake",
+        url: "http://battlesnake.checkfront.net/"
+    })
+
+    snakes.push({
+        name: "Giftbit Bounty Snake #1",
+        url: "http://bountysnake.lightraildev.net/ronda"
+    })
+
+    snakes.push({
+        name: "Giftbit Bounty Snake #2",
+        url: "http://bountysnake.lightraildev.net/rabble"
+    })
+
+    snakes.push({
+        name: "Semaphore Bounty Snake",
+        url: "https://salty-reef-19426.herokuapp.com/CCCCCC/10-100-0-0-0-0-0-0-0/agg-coo/"
+    })
+
+    snakes.push({
+        name: "Workday Bounty Snake #1",
+        url: "https://wd-battlesnake-bounty-2018-1.herokuapp.com/"
+    })
+
+    snakes.push({
+        name: "Workday Bounty Snake #2",
+        url: "https://wd-battlesnake-bounty-2018-2.herokuapp.com/"
+    })
+
+    snakes.push({
+        name: "Rooof Bounty Snake #1",
+        url: " http://adam.snake.rooof.com:8080/"
+    })
+
+    snakes.push({
+        name: "Rooof Bounty Snake #2",
+        url: "http://catlin.snake.rooof.com:8080/"
+    })
+
+    snakes.push({
+        name: "Bambora Bountyt Snake #1",
+        url: "http://battlesnake.na.bambora.com/sean"
+    })
+
+    snakes.push({
+        name: "Bambora Bountyt Snake #2",
+        url: "http://battlesnake.na.bambora.com/derrick"
+    })
+
+    snakes.push({
+        name: "Bambora Bountyt Snake #3",
+        url: "http://battlesnake.na.bambora.com/george"
+    })
+
+    snakes.push({
+        name: "Sendwithus Bounty Snake #1",
+        url: "http://35.185.235.22"
+    })
+
+    snakes.push({
+        name: "Sendwithus Bounty Snake #2",
+        url: "https://cubanboa.herokuapp.com/partyjem/"
+    })
+
     res.send(snakes)
 })
