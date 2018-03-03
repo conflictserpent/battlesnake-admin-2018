7edc8211a62a03ae0fcc41d39eb90b3c7b15367b
Daniel Steuernol <dlsteuer@gmail.com>
2018-03-03 13:34:15 -0800

    update rooof snake ports

--- client/src/components/data/userProvider.js
+++ client/src/components/data/userProvider.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import axios from "axios";
-import config from "../../config";
+import React, { Component } from 'react'
+import axios from 'axios'
+import config from '../../config'
 
 export const userProvider = ReactClass => {
   return class extends Component {
-    displayName = "userProvider";
+    displayName = 'userProvider';
     state = {
       loading: true,
       loggedIn: null,
@@ -17,34 +17,34 @@ export const userProvider = ReactClass => {
         this.setState({
           loading: false,
           loggedIn: false
-        });
-        window.location = `${config.SERVER}/auth/github`;
-        return;
+        })
+        window.location = `${config.SERVER}/auth/github`
+        return
       }
       this.setState({
         error: e,
         loading: false
-      });
+      })
     }
 
-    componentDidMount = async () => {
+    componentDidMount = async() => {
       try {
         const userResponse = await axios.get(`${config.SERVER}/api/self`, {
           withCredentials: true
-        });
-        axios.defaults.headers.common["x-csrf-token"] =
-          userResponse.headers.csrftoken;
-          
+        })
+        axios.defaults.headers.common['x-csrf-token'] =
+          userResponse.headers.csrftoken
+
         this.setState({
           loading: false,
           loggedIn: true,
           user: userResponse.data
-        });
+        })
       } catch (e) {
-        this.handleError(e);
+        this.handleError(e)
       }
     };
 
     render = () => <ReactClass userMgr={{ ...this.state }} />;
-  };
-};
+  }
+}
